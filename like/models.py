19004eab6f3311de6aeb745d79f59d612da2751f
kairat3 <jvckmiller@gmail.com>
2021-06-28 17:46:31 +0600

    added str to like models

--- like/models.py
+++ like/models.py
@@ -7,3 +7,6 @@ class Like(models.Model):
     owner = models.ForeignKey("user.CustomUser", on_delete=models.CASCADE, related_name='likes')
     post = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='likes')
     like = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.owner}-->{self.post}'
