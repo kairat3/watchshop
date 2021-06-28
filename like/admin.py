7dbc340a313cba01a86cd04b44eccfb6bc057ddf
kairat3 <jvckmiller@gmail.com>
2021-06-28 18:18:42 +0600

    deleted like

--- like/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from like.models import Like
-
-admin.site.register(Like)
--- whatchshop/settings.py
+++ whatchshop/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'user',
     'review',
     'products',
-    'like',
     'rest_framework',
     'corsheaders',
     'drf_yasg',
